49262df26fc066c893e6d1938c1e2a572b804e19
Bartosz Dobrosielski <dobry1@pm.me>
2021-12-04 12:30:16 +0100

    api

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import requests
-import json
-import datetime
-import uuid
-from pprint import pprint
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+import os
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
+token = 'ff23259d76db3cf6ef993524fd4a987019dd19f9e94c82ec'
 notebook_path = 'notebooks/one.ipynb'
 base = 'http://127.0.0.1:8888/'
-headers = {'Authorization': 'Token fc6628a48210a7fed3884b1f0fe8aa5b3da1c5f3c3218e15'
+headers = {'Authorization': 'Token ff23259d76db3cf6ef993524fd4a987019dd19f9e94c82ec'
            , 'Content-type': 'application/json'}
 
 # http://127.0.0.1:8888/?token=fc6628a48210a7fed3884b1f0fe8aa5b3da1c5f3c3218e15
 
 # api_url = 'http://127.0.0.1:8888/notebooks/one.ipynb'
-# token = 'fc6628a48210a7fed3884b1f0fe8aa5b3da1c5f3c3218e15'
+
 
 data = {}
 
-# r = requests.get(api_url, headers={
-#              'Authorization': 'token %s' % token,
-#             }
-#     )
 
 
-url = base + notebook_path
-response = requests.get(url, headers=headers)
-file = json.loads(response.text)
+# print("Button", runAll)
+# # r = requests.post(api_url)
 
-# r = requests.post(api_url)
+# print(response.url)
+# print(response.headers)
+# print(type(response))
 
-print(response.url)
-print(response.headers)
-print(type(response))
+
