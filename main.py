9739f9634083e96ce816bd603fce0093e5af44fd
Bartosz Dobrosielski <dobry1@pm.me>
2021-12-04 20:14:58 +0100

    draft

--- main.py
+++ main.py
@@ -1,32 +1,70 @@
+import json
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-import time
-import os
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+import datetime
+import uuid
+from pprint import pprint
+import socket
 
-token = 'ff23259d76db3cf6ef993524fd4a987019dd19f9e94c82ec'
-notebook_path = 'notebooks/one.ipynb'
-base = 'http://127.0.0.1:8888/'
-headers = {'Authorization': 'Token ff23259d76db3cf6ef993524fd4a987019dd19f9e94c82ec'
-           , 'Content-type': 'application/json'}
+import websocket
 
-# http://127.0.0.1:8888/?token=fc6628a48210a7fed3884b1f0fe8aa5b3da1c5f3c3218e15
-
-# api_url = 'http://127.0.0.1:8888/notebooks/one.ipynb'
+token = '240d1a6cca260512929e5a876a8a62fe0f34cc5c3560bb92'
+notebook_path = 'one.ipynb'
+base = 'http://127.0.0.1:8888'
+headers = {'Authorization': 'Token {}'.format(token),
+           'Content-type': 'application/json'}
 
 
-data = {}
+url = base + '/api/kernels'
+response = requests.post(url, headers=headers)
+kernel = json.loads(response.text)
+
+print('kernel: ', kernel['id'])
+
+# Load the notebook and get the code of each cell
+url = base + '/api/contents/' + notebook_path
+print("url: " + url)
+response = requests.get(url, headers=headers)
+file = json.loads(response.text)
+print("file: ", file)
+code = [c["source"] for c in file["content"]["cells"] if len(c["source"]) > 0]
+
+print(code)
+
+# Execution request/reply is done on websockets channels
+ws = websocket.WebSocket()
+
+ws.connect("ws://127.0.0.1:8888/api/kernels/" + kernel['id'] + "/channels",
+           header={'Authorization': 'Token 240d1a6cca260512929e5a876a8a62fe0f34cc5c3560bb92'})
+print(ws)
 
 
+def send_execute_request(code):
+    msg_type = 'execute_request';
+    content = {'code': code, 'silent': False}
+    hdr = {'msg_id': uuid.uuid1().hex,
+           'username': 'test',
+           'session': uuid.uuid1().hex,
+           'data': datetime.datetime.now().isoformat(),
+           'msg_type': msg_type,
+           'version': '5.0'}
+    msg = {'header': hdr, 'parent_header': hdr,
+           'metadata': {},
+           'content': content}
+    return msg
 
-# print("Button", runAll)
-# # r = requests.post(api_url)
 
-# print(response.url)
-# print(response.headers)
-# print(type(response))
+send_execute_request("hi there")
 
+for c in code:
+    ws.send(json.dumps(send_execute_request(c)))
 
+# We ignore all the other messages, we just get the code execution output
+# (this needs to be improved for production to take into account errors, large cell output, images, etc.)
+for i in range(0, len(code)):
+    msg_type = '';
+    while msg_type != "stream":
+        rsp = json.loads(ws.recv())
+        msg_type = rsp["msg_type"]
+    print(rsp["content"]["text"])
+
+ws.close()
